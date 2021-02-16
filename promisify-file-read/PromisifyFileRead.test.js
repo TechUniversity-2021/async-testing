3c48a3056f29e45c2b88e90d0b4f88fbbd045623
Anukriti <anukritigarg13@gmail.com>
2021-02-16 09:48:50 +0530

    feat(promisify-file-read): Add test for resolve using expect(promise).resolves

--- promisify-file-read/PromisifyFileRead.test.js
+++ promisify-file-read/PromisifyFileRead.test.js
@@ -8,10 +8,20 @@ test("Should resolve a promise to '21,34,43,57,'Anukriti'' ", function () {
 });
 
 
-test('Unit testing', function(done){
+test('Unit testing for resolved', function(done){
   jest.spyOn(fs, 'readFile').mockImplementation(function(file, option, callback){callback(null, '21,34,43,57,"Anukriti"')});
   promisifyFs('randomFile').then(function (text) {
       expect(text).toBe('21,34,43,57,"Anukriti"');
       done();
   });
 });
+
+test('Unit testing for resolved using .resolves', function(done){
+  jest.spyOn(fs, 'readFile').mockImplementation(function(file, option, callback){callback(null, '21,34,43,57,"Anukriti"')});
+  return expect(promisifyFs('randomFile').resolves.toBe('21,34,43,57,"Anukriti"'));
+});
+
+test('Unit testing for rejected',function(done){
+  jest.spyOn(fs, 'readFile').mockImplementation(function(file, option, callback){callback(null, '21,34,43,57,"Anukriti"')});
+
+})
