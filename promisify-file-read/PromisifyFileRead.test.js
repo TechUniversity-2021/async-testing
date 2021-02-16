5dc79e98045a79f48bdaf9919ee726fbe87b31c5
Anukriti <anukritigarg13@gmail.com>
2021-02-16 09:18:38 +0530

    feat(promisify-file-read): Add unit test by mocking fs.readFile function

--- promisify-file-read/PromisifyFileRead.test.js
+++ promisify-file-read/PromisifyFileRead.test.js
@@ -1,7 +1,17 @@
 const promisifyFs = require("./PromisifyFileRead");
+const fs = require('fs');
 
 test("Should resolve a promise to '21,34,43,57,'Anukriti'' ", () => {
     return expect(promisifyFs("./promisify-file-read/testFile.txt")).resolves.toBe(
       '21,34,43,57,"Anukriti"');
     //done();
+});
+
+
+test('Unit testing', (done) => {
+  jest.spyOn(fs, 'readFile').mockImplementation((file, option, callback) => callback(null, '21,34,43,57,"Anukriti"'));
+  promisifyFs('randomFile').then(text => {
+      expect(text).toBe('21,34,43,57,"Anukriti"');
+      done();
+  });
 });
