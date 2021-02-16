b3cf354e1c7a0ee29002578ff1c5f968afc52078
Anukriti <anukritigarg13@gmail.com>
2021-02-16 11:40:39 +0530

    refactor(sequence-file-read): Refactor SequenceFileRead.js to reduce lines of code

--- sequence-file-read/SequenceFileRead.js
+++ sequence-file-read/SequenceFileRead.js
@@ -8,9 +8,9 @@ function seqFileRead(filePath) {
     .then(function (data){
         return promisifyFs(data);    //./files/three.txt   returns This is your secret!
     })
-    .then(function (data) {
-        return data    //This is your secret
-    })
+    // .then(function (data) {
+    //     return data    //This is your secret
+    // })
     .catch(function (error){
         throw error;
         //return new Error("An error occured");
