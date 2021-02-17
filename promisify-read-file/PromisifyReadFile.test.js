b032ca7b0bb40b1975e2331da4c5c3f6ef4386ce
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-02-17 07:16:56 +0530

    feat(promisify-read-file): add unit testing by mocking fs.readFile

--- promisify-read-file/PromisifyReadFile.test.js
+++ promisify-read-file/PromisifyReadFile.test.js
@@ -1,6 +1,14 @@
 
-const PromisifyFs=require("./PromisifyReadFile")
-
+const promisifyFs=require("./PromisifyReadFile")
+const fs=require("fs")
 test("Should resolve promise to 'how are you?' ",()=>{
-    expect(PromisifyFs("./promisify-read-file/PromisifyReadFile")).resolves.toBe("how are you?")
+    expect(promisifyFs("./promisify-read-file/test.txt")).resolves.toBe("how are you?")
 })
+
+test('Unit testing by mocking', (done) => {
+    jest.spyOn(fs, 'readFile').mockImplementation((file, option, callback) => callback(null, 'how are you?'));
+    promisifyFs('abcd').then(text => {
+        expect(text).toBe('how are you?');
+        done();
+    });
+  }); 
